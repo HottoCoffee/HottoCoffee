ca0db8e4e5b3e0ae8a0c219cfc714d516189629f
T45K <tasktas9@gmail.com>
2024-02-17 17:47:43 +0900

    wip

--- backend/app/com/github/hottocoffee/controller/UserAuthController.scala
+++ backend/app/com/github/hottocoffee/controller/UserAuthController.scala
@@ -1,6 +1,6 @@
 package com.github.hottocoffee.controller
 
-import com.github.hottocoffee.controller.schema.response.{UserOutput, UserRegisterInput}
+import com.github.hottocoffee.controller.schema.response.{UserOutput, UserRegisterInput, UserSignInInput}
 import com.github.hottocoffee.util.value2Optional
 import jakarta.inject.{Inject, Singleton}
 import play.api.libs.json.{JsError, JsSuccess, Json}
@@ -27,6 +27,19 @@ class UserAuthController @Inject()(val controllerComponents: ControllerComponent
             .pipe(Created(_))
   }
 
-  def signIn(): Action[_] = Action {
-    Ok
+  def signIn(): Action[_] = Action { request =>
+    request.body.asJson match
+      case None => UnsupportedMediaType
+      case Some(json) => json.validate[UserSignInInput] match
+        case _: JsError => BadRequest
+        case JsSuccess(body, _) =>
+          UserOutput(
+            userId = 1,
+            accountId = "getupmax",
+            email = "hoge@example.com",
+            displayName = "tasuku_nakagawa",
+            introduction = "hoge",
+            iconUrl = "https://avatars.githubusercontent.com/u/38446259?v=4",
+          ).pipe(Json.toJson)
+            .pipe(Created(_))
   }
--- backend/app/com/github/hottocoffee/dao/UserDao.scala
+++ backend/app/com/github/hottocoffee/dao/UserDao.scala
@@ -1,7 +1,21 @@
 package com.github.hottocoffee.dao
 
-class UserDao {
+import anorm.{SQL, on}
+import jakarta.inject.Inject
+import play.db.Database
+
+class UserDao @Inject()(db: Database) {
   def selectByUserIds(userIds: List[Int]): List[UserRecord] = List(UserRecord(2))
+
+  def selectByEmailAndPassword(email: String, password: Array[Byte]): Option[UserRecord] = {
+    db.withConnection { implicit connection =>
+      SQL("select * from user where email = {email} and password = {password}")
+        .on(
+          "email" -> email,
+          "password" -> password,
+        )
+    }
+  }
 }
 
 case class UserRecord(id: Int)
