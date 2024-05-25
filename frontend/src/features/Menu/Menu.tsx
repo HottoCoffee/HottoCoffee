13c3dd9c86b2db36f1ccf2e839fcc1697a11b59a
seito2 <seito1201@gmail.com>
2024-05-25 14:31:40 +0900

    Add navigation

--- frontend/src/features/Menu/Menu.tsx
+++ frontend/src/features/Menu/Menu.tsx
@@ -3,69 +3,68 @@ import { useTransitionNavigate } from "@/hook/useTransitionNavigate";
 import { RiTimelineView } from "react-icons/ri";
 import { MdOutlinePostAdd } from "react-icons/md";
 import { useLocation } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+
+const PATHS = [
+  {
+    path: "/",
+    label: "タイムライン",
+    icon: <RiTimelineView size={20} />,
+  },
+  {
+    path: "/post",
+    label: "新規投稿",
+    icon: <MdOutlinePostAdd size={20} />,
+  },
+  {
+    path: "/profile",
+    label: "プロフィール",
+    icon: <CgProfile size={20} />,
+  },
+];
+
+const calcDirection = (
+  currentPath: string,
+  nextPath: string
+): "slide-to-left" | "slide-to-right" => {
+  const currentPathIndex = PATHS.findIndex((path) => path.path === currentPath);
+  const nextPathIndex = PATHS.findIndex((path) => path.path === nextPath);
+
+  return currentPathIndex > nextPathIndex ? "slide-to-right" : "slide-to-left";
+};
 
 export const Menu = () => {
   const { transitionNavigate } = useTransitionNavigate();
   const location = useLocation();
-  const path = location.pathname;
+  const currentPath = location.pathname;
 
   return (
     <nav
       className="flex justify-around items-center w-full bg-white rounded-lg shadow-md p-2 fixed bottom-3 m-auto left-0 right-0"
       style={{ viewTransitionName: "menu", maxWidth: "calc(100vw - 20px)" }}
     >
-      <Button
-        variant="link"
-        disabled={"/" === path}
-        onClick={() => {
-          const targetPath = "/";
+      {PATHS.map((path) => {
+        return (
+          <Button
+            variant="link"
+            disabled={currentPath === path.path}
+            onClick={() => {
+              if (currentPath === path.path) {
+                return;
+              }
 
-          if (targetPath === path) {
-            return;
-          }
-
-          transitionNavigate(targetPath);
-        }}
-        className="flex gap-2 items-center"
-      >
-        <RiTimelineView size={20} />
-        <span>timeline</span>
-      </Button>
-      <Button
-        variant="link"
-        disabled={"/post" === path}
-        onClick={() => {
-          const targetPath = "/post";
-
-          if (targetPath === path) {
-            return;
-          }
-
-          transitionNavigate(targetPath);
-        }}
-        className="flex gap-2 items-center"
-      >
-        <MdOutlinePostAdd size={20} />
-        <span>post</span>
-      </Button>
-      {/* <a
-        href="#"
-        className="flex flex-col items-center justify-center w-1/3 py-2 text-gray-700"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 mb-1"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
-        </svg>
-        <span>profile</span>
-      </a> */}
+              transitionNavigate(
+                path.path,
+                calcDirection(currentPath, path.path)
+              );
+            }}
+            className="flex gap-2 items-center"
+          >
+            {path.icon}
+            <span>{path.label}</span>
+          </Button>
+        );
+      })}
     </nav>
   );
 };
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { MainLayout } from "./features/MainLayout";
+import { ProfilePage } from "./pages/ProfilePage";
 
 const PostPage = lazy(() => import("./pages/PostPage"));
 const TimelinePage = lazy(() => import("./pages/TimelinePage"));
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
       {
         path: "/post",
         element: <PostPage />,
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
       },
     ],
   },
