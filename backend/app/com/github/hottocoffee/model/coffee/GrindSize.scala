88ab513285ba301ce8ab5e14e60a37ef5e873a1a
T45K <tasktas9@gmail.com>
2024-09-14 16:52:16 +0900

    fix enum stuff

--- backend/app/com/github/hottocoffee/model/coffee/GrindSize.scala
+++ backend/app/com/github/hottocoffee/model/coffee/GrindSize.scala
@@ -1,12 +1,16 @@
 package com.github.hottocoffee.model.coffee
 
-enum GrindSize:
-  case finest, fine, medium_fine, medium, coarse
+enum GrindSize(val value: String):
+  case FINEST extends GrindSize("finest")
+  case FINE extends GrindSize("fine")
+  case MEDIUM_FINE extends GrindSize("medium_fine")
+  case MEDIUM extends GrindSize("medium")
+  case COARSE extends GrindSize("coarse")
 
 object GrindSize:
   def apply(value: String): Either[Unit, GrindSize] = {
     val trim = value.trim
-    GrindSize.values.find(_.toString == trim) match
-      case Some(value) => Right(value)
+    GrindSize.values.find(_.value == trim) match
+      case Some(it) => Right(it)
       case None => Left(())
   }
