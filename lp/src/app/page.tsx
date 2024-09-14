08b5fc2a2cd05c096e3358dd5441edd264d00290
seito2 <seito1201@gmail.com>
2024-09-14 14:38:59 +0900

    Add meta

--- lp/src/app/page.tsx
+++ lp/src/app/page.tsx
@@ -1,4 +1,10 @@
 import LP from "@/components/lp";
+import type { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "HottoCoffee",
+  keywords: "coffee,log,blog,note",
+};
 
 export default function Home() {
   return (
