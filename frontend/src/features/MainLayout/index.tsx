af9c9617c0ee2238462908939878f9024b0a1ba0
seito2 <seito1201@gmail.com>
2024-09-14 15:31:02 +0900

    Fix loading

--- frontend/src/features/MainLayout/index.tsx
+++ frontend/src/features/MainLayout/index.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Menu } from "../Menu/Menu";
 import { Suspense } from "react";
+import { IoIosCafe } from "react-icons/io";
 
 export const MainLayout = () => {
   return (
     <div className="pb-32">
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <div className="w-screen h-screen flex justify-center items-center">
+            <IoIosCafe size={52} className="text-amber-700" />
+          </div>
+        }
+      >
         <Outlet />
       </Suspense>
 
