7d81e5f8772aef560f70951a0468b1c7b6d2a9d4
T45K <tasktas9@gmail.com>
2025-01-22 19:51:03 +0900

    add comment to DB column

--- backend/conf/evolutions/default/6.sql
+++ backend/conf/evolutions/default/6.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 alter table user
     drop icon_url,
-    add icon_image_key varchar(36) after introduction;
+    add icon_image_key varchar(36) comment 'UUID v4' after introduction;
 
 # --- !Downs
 alter table user
